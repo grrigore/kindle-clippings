b9381207331a2cdc8052b9a8a2ae2fb1f31344a6
Grigore Cristian-Andrei <cristianandreigrigore@gmail.com>
2019-04-11 01:53:35 +0300

    get highlights into list

--- reorganize.py
+++ reorganize.py
@@ -3,8 +3,20 @@ import sys
 ### MyClippings.txt file path
 fileName = sys.argv[1]
 
-with open(fileName) as myClippingsFile:
-    line = myClippingsFile.readline()
-    while line:
-	print(line)
-        line = myClippingsFile.readline()
+highlightSeparator = "==========\n"
+highlights = []
+
+with open(fileName, 'r+') as myClippingsFile:
+    data = myClippingsFile.read()
+    myClippingsFile.seek(0)
+    myClippingsFile.write(highlightSeparator + data)
+    highlights = data.split(highlightSeparator)
+    highlights = highlights[:-1]
+    for highlight in highlights:
+        print(highlight + " ------ ")
+
+for highlight in highlights:
+    bookTitle = highlight.split("(")[0]
+    bookTitle = bookTitle[:-1] + '.txt'
+    with open(bookTitle, 'a+') as bookHighlights:
+        bookHighlights.write(highlight)
