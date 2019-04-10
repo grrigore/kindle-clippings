1e5d3638e4501e846f01255529266fb013b96e50
Grigore Cristian-Andrei <cristianandreigrigore@gmail.com>
2019-04-10 21:50:17 +0300

    restart

--- reorganize.py
+++ reorganize.py
@@ -5,16 +5,6 @@ fileName = sys.argv[1]
 
 with open(fileName) as myClippingsFile:
     line = myClippingsFile.readline()
-    firstLine = line
-    cnt = 1
     while line:
+	print(line)
         line = myClippingsFile.readline()
-        if line == "==========\n":
-            with open(firstLine + '.txt', 'a') as bookFile:
-                bookFile.write('Hello\n')
-            bookFile = myClippingsFile.readline()
-        else:
-            break
-       # print("Line {}: {}".format(cnt, line.strip()))
-       # line = myClippingsFile.readline()
-       # cnt += 1
